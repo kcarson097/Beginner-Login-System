7aa98665cdfa1b219fe2e5ede014b384982d095f
kcarson097 <63301343+kcarson097@users.noreply.github.com>
2020-07-14 19:03:39 +0100

    Add files via upload

---
+++ Random-Jokes-Webscraper.py
@@ -0,0 +1,24 @@
+import random
+import praw
+#random joke generator
+from tkinter import *
+
+root = Tk()
+
+def random_joke():
+    reddit = praw.Reddit(client_id = 'B69mb6gi7dkJrg',client_secret = 'ln-itHfMFQblfCCTFmguh9Ok3b0', user_agent = 'Kyle' )
+    jokes_list = []
+    jokes = reddit.subreddit('Oneliners').hot(limit=100)
+    for post in jokes:
+        jokes_list.append(post.title)
+    
+    answer = random.choice(jokes_list)
+    label = Label(text = answer)
+    label.pack()
+
+myButton = Button(text = 'Random Joke', command = random_joke)
+myButton.pack()
+
+
+root.mainloop()
+
