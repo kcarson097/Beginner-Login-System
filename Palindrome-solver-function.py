d2a31f12774f3e23a9b23525de8c89c2aea32acc
kcarson097 <63301343+kcarson097@users.noreply.github.com>
2020-08-11 13:44:33 +0100

    Delete Palindrome-solver-function.py

--- Palindrome-solver-function.py
+++
@@ -1,4 +0,0 @@
-#**Check if Palindrome** - Checks if the string entered by the user is a palindrome. That is that it reads the same forwards as backwards like “racecar”
-
-def check_palindrome(string):
-    return string[::] == string[::-1] 
